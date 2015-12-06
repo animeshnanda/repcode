03caeb1fbe5bb890b5984d7224e4748b567cad2d
animeshnanda <animeshnanda@gmail.com>
2015-12-06 17:17:16 +0530

    Review Modal Layout Fix

--- web/common/commonService.js
+++ web/common/commonService.js
@@ -43,6 +43,16 @@ define([
 			return promise;
 		};
 	})
+	.service('reviewService', function($http,$q) {
+		this.getChefReview = function() {
+			var strData;
+			var promise=$http.get("http://localhost:9000/data/review-data.json")
+			.success(function (data) {
+				strData = data;
+			});
+			return promise;
+		};
+	})
 	.factory('location', [function() {
 		var service = {
 		  isReady: false,
--- web/modules/mchef/chefCtrl.js
+++ web/modules/mchef/chefCtrl.js
@@ -16,10 +16,9 @@ define([
 			});
 	}])
 
-	.controller('chefCtrl', ['$scope','$http','$rootScope','$document','location','cheflistService',function($scope,$http,$rootScope,$document,location,cheflistService) {
+	.controller('chefCtrl', ['$scope','$http','$rootScope','$document','location','cheflistService','reviewService',function($scope,$http,$rootScope,$document,location,cheflistService,reviewService) {
 		var chefData = cheflistService.getChefList().then(function(data){
 			$scope.cheflistdata = data.data;
-			//console.log($scope.lcombos);
 		});
 		location.get(angular.noop, angular.noop);
 		$scope.isModalVisible = false;
@@ -31,6 +30,9 @@ define([
 		$scope.$watch('pickedLocation', $scope.toggleModal);
 		$scope.$watch('lookedUpLocation', $scope.toggleModal);
 		
+		var chefReviewData = reviewService.getChefReview().then(function(data){
+			$scope.chefreviewdata = data.data;
+		});		
 		$scope.showModal = false;
 		$scope.toggleModal = function(){
 			$scope.showModal = !$scope.showModal;
